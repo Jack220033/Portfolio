e8e2ab3b491af67734536b4df6a621e3a36c48c3
Jack220033 <103004263+Jack220033@users.noreply.github.com>
2023-05-04 17:43:04 -0400

    Delete Portfolio-Jacob-Arcand directory

--- Portfolio-Jacob-Arcand/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Footer.module.css';
-
-export default function Footer() {
-    return <footer className={styles.footer}>
-        <div className="wrapper">
-            &copy; Copyright Jacob Arcand
-        </div>
-    </footer>
-}
